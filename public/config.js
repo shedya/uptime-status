bf471435e7d2e65d3eefc48ed06d35b288dd9be0
pad4-org <112143366+pad4-org@users.noreply.github.com>
2022-09-25 17:29:05 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,19 +1,19 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'BantkSystem-状态监测',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm792742102-cc5d3feb85a3a8850016148e',
+    'm792742117-3bacbf76e88bca40aa45bbc0',
+    'm792742112-9b1e895e05e7bfca9b42ef8d',
+    'm792742120-1121d8437172cb2988fcbbbe',
   ],
 
   // 日志天数
-  CountDays: 90,
+  CountDays: 7,
 
   // 是否显示检测站点的链接
   ShowLink: true,
@@ -21,16 +21,16 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: '首页',
+      url: 'https://web.bantk.com'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      text: '听音乐',
+      url: 'https://yy.pad4.org'
     },
     {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: 'bantk',
+      url: 'https://bantk.com/'
     },
   ],
 };
