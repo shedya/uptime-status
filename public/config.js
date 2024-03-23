122966e190c2c59c5280744f82ebb8891ac7b59e
SHED <53966497+shedya@users.noreply.github.com>
2024-03-24 03:17:12 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -6,14 +6,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm792742102-cc5d3feb85a3a8850016148e',
-    'm792743025-4256e47adaa0b22f662fe019',
-    'm792742117-3bacbf76e88bca40aa45bbc0',
-    'm792742112-9b1e895e05e7bfca9b42ef8d',
-    'm792742120-1121d8437172cb2988fcbbbe',
-    'm792742379-d82456f7c8b9aa4f73856410',
-    'm792742383-dd39c79911c76f19081ddd10',
-    
+    'ur1329066-faefb8db0b2ef561934e266c',
   ],
 
   // 日志天数
