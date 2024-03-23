62f50da65404e040fa721d0c339a913eaa018de8
SHED <53966497+shedya@users.noreply.github.com>
2024-03-24 03:18:32 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -7,6 +7,11 @@ window.Config = {
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
     'ur1329066-faefb8db0b2ef561934e266c',
+    'm784488775-dd1ad84b209c05f8e185c33e',
+    'm784490063-7b5da437e7f1e0d67613714d',
+    'm784497419-de55aa09902ccb3ab22d548a',
+    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm784496436-71a4bf7b1e3bdf7756be131b',
   ],
 
   // 日志天数
